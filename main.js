4a191c0c78746d38715151b6e71a25381a638482
massi <massimo.battinelli@gmail.com>
2022-03-05 12:56:55 +0100

    fine progettoo

--- main.js
+++ main.js
@@ -14,3 +14,25 @@ console.log(numberToRemember);
 alert("Memorizza questi numeri\n" + numberToRemember + "\n");
 //richiamo funzione dopo 30 secondi
 setTimeout(checkNumber, 3000);
+
+function checkNumber() {
+    //richiesta inerimento numeri da utente
+    var numberToCheck = [];
+    for (var i = 0; i < quantiNumeri; i++) {
+        number = parseInt(prompt("Inserisci un numero alla volta quello cher ti ricordi"));
+        numberToCheck.push(number);
+    }
+    console.log(numberToCheck);
+    var correctNumber = [];
+    var counter = 0;
+    //controllo numeri corretti
+    for (var i=0; i<quantiNumeri; i++) {
+        if (numeriEsatti.includes(numberToCheck[i])) {
+            correctNumber.push(numberToCheck[i])
+            counter ++;
+        }
+    }
+    console.log(correctNumber);
+    var messaggioNumeriCorretti = "Hai inserito correttemante " + counter + " numeri" + "\n" + correctNumber;
+    alert(messaggioNumeriCorretti);
+}
